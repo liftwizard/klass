67973ed119bbdeb54ce177a93d2944b23f447343
Craig P. Motlin <cmotlin@gmail.com>
2025-05-29 17:27:28 +0000

    Add multirepo-migration.klass to KlassCompilerTest.java.

--- klass-model-converters/klass-compiler-tests/src/test/java/cool/klass/model/converter/compiler/KlassCompilerTest.java
+++ klass-model-converters/klass-compiler-tests/src/test/java/cool/klass/model/converter/compiler/KlassCompilerTest.java
@@ -61,6 +61,11 @@ class KlassCompilerTest {
 		this.assertNoCompilerErrors("/cool/klass/xample/coverage/coverage-example.klass");
 	}
 
+	@Test
+	void multirepoMigration() {
+		this.assertNoCompilerErrors("multirepo-migration.klass");
+	}
+
 	private void assertNoCompilerErrors(@Nonnull String sourceCodeName) {
 		String sourceCodeText = FileSlurper.slurp(sourceCodeName, this.getClass());
 		CompilationUnit compilationUnit = CompilationUnit.createFromText(
