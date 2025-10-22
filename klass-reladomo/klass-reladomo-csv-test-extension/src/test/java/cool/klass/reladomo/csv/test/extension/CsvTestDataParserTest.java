ebd33e89a679b15b58b4d218fe6fa69362a42aa9
Craig P. Motlin <cmotlin@gmail.com>
2025-10-22 14:33:18 -0400

    Add klass-reladomo-csv-test-extension module for CSV-based test data loading.

---
+++ klass-reladomo/klass-reladomo-csv-test-extension/src/test/java/cool/klass/reladomo/csv/test/extension/CsvTestDataParserTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2025 Craig Motlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cool.klass.reladomo.csv.test.extension;
+
+import java.util.List;
+
+import com.gs.fw.common.mithra.attribute.Attribute;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CsvTestDataParserTest {
+
+	@Test
+	void extractClassNameFromFilename() {
+		CsvTestDataParser parser = new CsvTestDataParser("test-data/cool.klass.xample.coverage.User.csv");
+		assertThat(parser.getClassName()).isEqualTo("cool.klass.xample.coverage.User");
+	}
+
+	@Test
+	void extractClassNameFromFilenameWithoutPath() {
+		CsvTestDataParser parser = new CsvTestDataParser("test-data/cool.klass.xample.coverage.User.csv");
+		assertThat(parser.getClassName()).isEqualTo("cool.klass.xample.coverage.User");
+	}
+
+	@Test
+	void throwsExceptionForNonCsvFile() {
+		assertThatThrownBy(() -> new CsvTestDataParser("test-data/User.txt"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("Filename must end with .csv");
+	}
+
+	@Test
+	void parsesAttributesFromHeaders() {
+		CsvTestDataParser parser = new CsvTestDataParser("test-data/cool.klass.xample.coverage.User.csv");
+		List<Attribute<?, ?>> attributes = parser.getAttributes();
+
+		assertThat(attributes).hasSize(6);
+		assertThat(attributes.get(0).getAttributeName()).isEqualTo("systemFrom");
+		assertThat(attributes.get(1).getAttributeName()).isEqualTo("systemTo");
+		assertThat(attributes.get(2).getAttributeName()).isEqualTo("userId");
+		assertThat(attributes.get(3).getAttributeName()).isEqualTo("firstName");
+		assertThat(attributes.get(4).getAttributeName()).isEqualTo("lastName");
+		assertThat(attributes.get(5).getAttributeName()).isEqualTo("email");
+	}
+
+	@Test
+	void createsDataObjects() {
+		CsvTestDataParser parser = new CsvTestDataParser("test-data/cool.klass.xample.coverage.User.csv");
+		assertThat(parser.getDataObjects()).hasSize(2);
+	}
+}
