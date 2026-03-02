8b72ef059c3303edb68a990d3edcf220e276580e
Craig P. Motlin <cmotlin@gmail.com>
2026-03-02 00:07:31 +0000

    [fail] [probably] Add DELETE endpoints for meta model types with cascade delete support.

--- klass-reladomo/klass-reladomo-persistent-writer/src/main/java/cool/klass/reladomo/persistent/writer/PersistentDeleter.java
+++ klass-reladomo/klass-reladomo-persistent-writer/src/main/java/cool/klass/reladomo/persistent/writer/PersistentDeleter.java
@@ -16,12 +16,16 @@
 
 package cool.klass.reladomo.persistent.writer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.annotation.Nonnull;
 
 import cool.klass.data.store.DataStore;
 import cool.klass.model.meta.domain.api.Klass;
+import cool.klass.model.meta.domain.api.property.AssociationEnd;
+import cool.klass.model.meta.domain.api.property.ReferenceProperty;
 
 public class PersistentDeleter {
 
@@ -36,6 +40,69 @@ public class PersistentDeleter {
 	}
 
 	public void deleteOrTerminate(Klass klass, @Nonnull Object persistentInstance) {
+		List<OrphanedChild> orphanedChildren = new ArrayList<>();
+		this.collectOrphanedChildrenRecursive(klass, persistentInstance, orphanedChildren);
+
 		this.dataStore.deleteOrTerminate(persistentInstance);
+
+		for (OrphanedChild orphan : orphanedChildren) {
+			this.deleteOrTerminate(orphan.klass(), orphan.instance());
+		}
 	}
+
+	private void collectOrphanedChildrenRecursive(
+		Klass klass,
+		Object persistentInstance,
+		List<OrphanedChild> orphanedChildren
+	) {
+		this.collectOrphanedChildrenAtLevel(klass, persistentInstance, orphanedChildren);
+
+		for (Klass subClass : klass.getSubClasses()) {
+			Object subClassInstance = this.dataStore.getSubClass(persistentInstance, klass, subClass);
+			if (subClassInstance != null) {
+				this.collectOrphanedChildrenRecursive(subClass, subClassInstance, orphanedChildren);
+			}
+		}
+	}
+
+	private void collectOrphanedChildrenAtLevel(
+		Klass klass,
+		Object persistentInstance,
+		List<OrphanedChild> orphanedChildren
+	) {
+		var fkEnds = klass.getForeignKeys().keysView().toSet();
+
+		for (AssociationEnd ownedEnd : klass.getAssociationEnds().select(ReferenceProperty::isOwned)) {
+			if (ownedEnd.getOwningClassifier() != klass) {
+				continue;
+			}
+			if (fkEnds.contains(ownedEnd)) {
+				if (ownedEnd.getMultiplicity().isToOne()) {
+					Object child = this.dataStore.getToOne(persistentInstance, ownedEnd);
+					if (child != null) {
+						orphanedChildren.add(new OrphanedChild(ownedEnd.getType(), child));
+					}
+				} else {
+					List<Object> children = this.dataStore.getToMany(persistentInstance, ownedEnd);
+					for (Object child : children) {
+						orphanedChildren.add(new OrphanedChild(ownedEnd.getType(), child));
+					}
+				}
+			} else {
+				if (ownedEnd.getMultiplicity().isToOne()) {
+					Object child = this.dataStore.getToOne(persistentInstance, ownedEnd);
+					if (child != null) {
+						this.collectOrphanedChildrenRecursive(ownedEnd.getType(), child, orphanedChildren);
+					}
+				} else {
+					List<Object> children = this.dataStore.getToMany(persistentInstance, ownedEnd);
+					for (Object child : children) {
+						this.collectOrphanedChildrenRecursive(ownedEnd.getType(), child, orphanedChildren);
+					}
+				}
+			}
+		}
+	}
+
+	private record OrphanedChild(Klass klass, Object instance) {}
 }
