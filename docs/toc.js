a547e87e446f730c3dc19f0ce2ac88bfe525f734
motlin <motlin@users.noreply.github.com>
2026-06-12 03:22:26 +0000

    [skip ci] deploy to gh-pages ad2e9a00f05f95c1e074d3c5b17430107dcbc6cd

---
+++ docs/toc.js
@@ -0,0 +1,206 @@
+toc = [ {
+  "chapterTitle" : "",
+  "dirName" : "",
+  "items" : [ {
+    "chapterTitle" : "",
+    "pageTitle" : "",
+    "pageMeta" : { },
+    "dirName" : "",
+    "fileName" : "index",
+    "fileExtension" : "",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Klass",
+      "id" : "klass",
+      "customAnchorId" : "klass"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Vision",
+  "dirName" : "vision",
+  "items" : [ {
+    "chapterTitle" : "Vision",
+    "pageTitle" : "Overview",
+    "pageMeta" : { },
+    "dirName" : "vision",
+    "fileName" : "overview",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Vision",
+    "pageTitle" : "Classes",
+    "pageMeta" : { },
+    "dirName" : "vision",
+    "fileName" : "classes",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Vision",
+    "pageTitle" : "Associations",
+    "pageMeta" : { },
+    "dirName" : "vision",
+    "fileName" : "associations",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Vision",
+    "pageTitle" : "Projections And Services",
+    "pageMeta" : { },
+    "dirName" : "vision",
+    "fileName" : "projections-and-services",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Audit Data",
+  "dirName" : "audit-data",
+  "items" : [ {
+    "chapterTitle" : "Audit Data",
+    "pageTitle" : "Overview",
+    "pageMeta" : { },
+    "dirName" : "audit-data",
+    "fileName" : "overview",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Audit Data",
+      "id" : "audit-data",
+      "customAnchorId" : "audit-data"
+    } ]
+  }, {
+    "chapterTitle" : "Audit Data",
+    "pageTitle" : "Audit Model",
+    "pageMeta" : { },
+    "dirName" : "audit-data",
+    "fileName" : "audit-model",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Audit Data",
+    "pageTitle" : "Audited Services",
+    "pageMeta" : { },
+    "dirName" : "audit-data",
+    "fileName" : "audited-services",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Bootstrapped Metamodel",
+  "dirName" : "bootstrapped-metamodel",
+  "items" : [ {
+    "chapterTitle" : "Bootstrapped Metamodel",
+    "pageTitle" : "Overview",
+    "pageMeta" : { },
+    "dirName" : "bootstrapped-metamodel",
+    "fileName" : "overview",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Bootstrapped Metamodel",
+      "id" : "bootstrapped-metamodel",
+      "customAnchorId" : "bootstrapped-metamodel"
+    } ]
+  }, {
+    "chapterTitle" : "Bootstrapped Metamodel",
+    "pageTitle" : "Bootstrapped Model Data",
+    "pageMeta" : { },
+    "dirName" : "bootstrapped-metamodel",
+    "fileName" : "bootstrapped-model-data",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Bootstrapped Metamodel",
+    "pageTitle" : "Bootstrapped Meta Model Data",
+    "pageMeta" : { },
+    "dirName" : "bootstrapped-metamodel",
+    "fileName" : "bootstrapped-meta-model-data",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Bootstrapped Metamodel",
+    "pageTitle" : "Dynamic Query",
+    "pageMeta" : { },
+    "dirName" : "bootstrapped-metamodel",
+    "fileName" : "dynamic-query",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Bootstrapped Metamodel",
+    "pageTitle" : "Dynamic Ui",
+    "pageMeta" : { },
+    "dirName" : "bootstrapped-metamodel",
+    "fileName" : "dynamic-ui",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Bootstrapped Metamodel",
+    "pageTitle" : "Dynamic Model",
+    "pageMeta" : { },
+    "dirName" : "bootstrapped-metamodel",
+    "fileName" : "dynamic-model",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Additional Features",
+  "dirName" : "additional-features",
+  "items" : [ {
+    "chapterTitle" : "Additional Features",
+    "pageTitle" : "Overview",
+    "pageMeta" : { },
+    "dirName" : "additional-features",
+    "fileName" : "overview",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Additional Features",
+      "id" : "additional-features",
+      "customAnchorId" : "additional-features"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Alternative Implementations",
+  "dirName" : "alternative-implementations",
+  "items" : [ {
+    "chapterTitle" : "Alternative Implementations",
+    "pageTitle" : "Overview",
+    "pageMeta" : { },
+    "dirName" : "alternative-implementations",
+    "fileName" : "overview",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Alternative implementation choices",
+      "id" : "alternative-implementation-choices",
+      "customAnchorId" : "alternative-implementation-choices"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Related Works",
+  "dirName" : "related-works",
+  "items" : [ {
+    "chapterTitle" : "Related Works",
+    "pageTitle" : "Overview",
+    "pageMeta" : { },
+    "dirName" : "related-works",
+    "fileName" : "overview",
+    "fileExtension" : "md",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Related Works",
+      "id" : "related-works",
+      "customAnchorId" : "related-works"
+    } ]
+  } ]
+} ]
